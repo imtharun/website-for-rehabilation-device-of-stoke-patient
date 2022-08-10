c565d88b69fb69eb634a602c3a02d433fdfd72b9
Suryah07 <suryahdhamodharan@gmail.com>
2022-08-10 19:37:48 +0530

    initial

---
+++ backend/database.js
@@ -0,0 +1,65 @@
+const mysql = require('mysql');
+const config = require('./config_sql');
+const con = mysql.createConnection({
+    host: config.host,
+    user: config.uname,
+    password: config.upass,
+    database: config.database
+});
+
+connect();
+//used to establish connection with the database
+function connect()
+{
+    con.connect(function(err) 
+    {
+        if (err) throw err;
+        console.log("database Connected!");
+    });
+}
+
+
+function authorise(username , password)
+{
+    let results;
+    sql = 'SELECT password from auth where user_id = ?';
+    const value = [username];
+    console.log(value);
+    con.query(sql,value,(err,result)=>
+    {
+        if(result.length===0)
+        {
+            results = "denied";
+            callback(err,results);
+            return;
+        }
+        else
+        {
+        const  resultArray = Object.values(JSON.parse(JSON.stringify(result))[0])[0];
+        if(password === resultArray)
+        {
+            results = "granted";
+        }
+        else
+        {
+            results = "denied";
+        }
+        callback(err,results);
+    }
+    })
+}
+
+function createuser(username,password){
+
+}
+
+
+
+
+
+
+module.exports = {
+    authorise,
+    createuser,
+    
+};
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,39 @@
-console.log("Hello world");
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors")
+const db = require('./database')
+
+//port number to listen
+const port = 5000;
+
+//init
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+
+//initializing
+app.listen(port,()=>{
+  console.log("Server starten to listen...");
+}); 
+
+
+//home page 
+app.get('/', function(req, res){
+  res.send("Only accepting GET and POST requests!");
+});
+
+app.post('/auth',function(req,res){
+    const mailid = req.body.mailid;
+    const password = req.body.password;
+    const resul =db.authoriseuser(username,password,(err,result)=>{
+        if(err) console.log(err);
+        acces = result;
+        console.log(acces);
+        res.send({
+          access: acces,
+          user: rep,
+        });
+    })
+    res.send(resul);
+})
